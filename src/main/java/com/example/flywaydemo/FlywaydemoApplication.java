908aee0b64ff51b1076757861a3761015c3c9c1a
Rajesh Gedela <rajesh.gedela@philips.com>
2019-05-22 11:25:39 -0400

    new files

---
+++ src/main/java/com/example/flywaydemo/FlywaydemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.flywaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywaydemoApplication.class, args);
+		System.out.println("Print me");
+	}
+
+}
